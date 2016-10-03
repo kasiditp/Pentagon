289b91cd79872e96c851d46577ee867e52d6b80e
devilmustcry <eaglization@gmail.com>
2016-10-03 15:24:50 +0700

    Create product app and product model

---
+++ product/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from product.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin) :
+    list_display = ('id','name', 'type', 'sex', 'brand', 'type', 'picture')
+    search_fields = ['name']
+
+
+admin.site.register(Product, ProductAdmin)
+
---
+++ product/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+PRODUCT_TYPE = [
+    (1, "Top"),
+    (2, "Bottom"),
+    (3, "Overall"),
+    (4, "Shoe"),
+    (5, "Accessory")
+]
+
+SEX = [
+    (1, "Men"),
+    (2, "Women"),
+    (3, "Unisex")
+]
+
+# Create your models here.
+class Product(models.Model) :
+    name = models.CharField(verbose_name="Name", max_length=64, null=False, blank=False)
+    type = models.IntegerField(verbose_name="Product Type", choices=PRODUCT_TYPE, null=True)
+    sex = models.IntegerField(verbose_name="Sex", choices=SEX, null=False)
+    brand = models.CharField(verbose_name="Brand", max_length=64, null=False, blank=True)
+    size = models.CharField(verbose_name="Size", max_length=8, null=False, blank=False)
+    picture = models.CharField(verbose_name="Picture", max_length=256, null=True, blank=True)
+
+
+
