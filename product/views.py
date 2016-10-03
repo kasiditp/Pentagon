77c2a55d5e6a98521768b4df103b85e928ae270e
devilmustcry <eaglization@gmail.com>
2016-10-03 18:00:39 +0700

    Edit product model (Add description & price) & add product_view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from product.models import Product
+
+
+def product_view(request) :
+    product_list = Product.objects.all()
+
+    context = {
+        'product_list' : product_list
+    }
+
+    return render(request, "product/product.html", context)
+
+# type brand search
+
