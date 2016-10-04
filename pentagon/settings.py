f194caa641d609bafa41bfb2261697173d221537
Pitchaya Namchaisiri <pea14733@gmail.com>
2016-10-04 12:56:09 +0700

    Added humanize app. Product image is now zoomable.

--- pentagon/settings.py
+++ pentagon/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'product'
+    'product',
+    'django.contrib.humanize'
 )
 
 MIDDLEWARE_CLASSES = (
