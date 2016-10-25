f67126c47bf3f5328402d19a82dcebe2d0bd266d
Zen <zen23139@gmail.com>
2016-10-25 17:58:35 +0700

    registration system with condition prevention V1

--- member/views.py
+++ member/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from django.urls import reverse
 from models import User
 import hashlib
@@ -19,6 +19,7 @@ def add_new_user(request):
         unique_id = '00001'
         username = request.POST['username']
         password = request.POST['password']
+        re_password = request.POST['re_password']
         sex = 2
         if request.POST['gender'] == 'male':
             sex = 1
@@ -29,10 +30,15 @@ def add_new_user(request):
         email = request.POST['email']
         address = request.POST['address']
 
+        if  username == '' or password == '' or re_password == '' or first_name == '' or last_name == '' or birthdate == '' or email == '' or address == '':
+            return HttpResponseRedirect(request, 'pages/member/register.html' , {"error":"Please input your information to all fields!"})
+        if password != re_password:
+            return HttpResponseRedirect(request, 'pages/member/register.html', {"error": "Password and confirm password field must be the same!"})
+
         new_user = User.objects.create(unique_id = unique_id , username = username ,password = hashlib.md5(password).hexdigest(),email=email,sex=sex,birth_date=birthdate,first_name=first_name,last_name=last_name,address=address)
         new_user.save()
 
-        #if error occur return render(request, 'pages/member/register.html' , {"error":"some message!!!"})
+
 
     return render(request, 'pages/member/register.html')
 
